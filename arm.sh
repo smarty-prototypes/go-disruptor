f06e22b11b279d380d867e36e81add34cb16b7d8
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-14 16:14:48 -0600

    Shell script to run on ARM-based (android) hardware.

---
+++ arm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+GOOS=linux GOARCH=arm GOARM=7 go build
+~/Downloads/android/adt-bundle-mac-x86_64-20140321/sdk/platform-tools/adb push go-disruptor /data/local/tmp
+~/Downloads/android/adt-bundle-mac-x86_64-20140321/sdk/platform-tools/adb shell "chmod 755 /data/local/tmp/go-disruptor"
+~/Downloads/android/adt-bundle-mac-x86_64-20140321/sdk/platform-tools/adb shell "/data/local/tmp/go-disruptor"
