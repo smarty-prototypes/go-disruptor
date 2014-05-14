a0e10b6830bd6200ef6109bed9b2830c020407d4
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-14 16:18:01 -0600

    Make files for ARM and Intel.

--- run-arm.sh
+++ run-arm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 GOOS=linux GOARCH=arm GOARM=7 go build
 ~/Downloads/android/adt-bundle-mac-x86_64-20140321/sdk/platform-tools/adb push go-disruptor /data/local/tmp
 ~/Downloads/android/adt-bundle-mac-x86_64-20140321/sdk/platform-tools/adb shell "chmod 755 /data/local/tmp/go-disruptor"
