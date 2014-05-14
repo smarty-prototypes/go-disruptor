6cf8dcc17ff0b527668da425c64bd94f910f60b8
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-13 21:18:23 -0600

    Benchmarking sequencer.

--- single_sequencer_test.go
+++ single_sequencer_test.go
@@ -2,6 +2,23 @@ package main
 
 import "testing"
 
+func BenchmarkSingleProducerSequencerNext(b *testing.B) {
+	consumerSequence := NewSequence()
+	publisherSequence := NewSequence()
+	consumerBarrier := NewBarrier(consumerSequence)
+
+	sequencer := NewSingleProducerSequencer(publisherSequence, 1024, consumerBarrier)
+	iterations := int64(b.N)
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := int64(0); i < iterations; i++ {
+		claimed := sequencer.Next(1)
+		//consumerSequence[0] = claimed
+		consumerSequence.Store(claimed)
+	}
+}
+
 func BenchmarkSingleProducerSequencerPublish(b *testing.B) {
 	sequencer := NewSingleProducerSequencer(NewSequence(), 1024, Barrier{})
 	iterations := int64(b.N)
