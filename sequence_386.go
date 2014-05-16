9275f399a36a196279ed802d0e0519b10296993f
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-16 17:09:41 -0600

    Corrected syntax for 386 and ARM builds.

--- sequence_386.go
+++ sequence_386.go
@@ -3,7 +3,7 @@ package disruptor
 import "sync/atomic"
 
 func (this *Sequence) Store(value int64) {
-	atomic.StoreInt64(&this.value)
+	atomic.StoreInt64(&this.value, value)
 }
 func (this *Sequence) Load() int64 {
 	return atomic.LoadInt64(&this.value)
