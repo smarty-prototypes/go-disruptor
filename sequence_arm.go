f2324445a549e2ebee414ebbfcd66c03f2474714
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-16 11:02:05 -0600

    ARM is 64 byte as well.

--- sequence_arm.go
+++ sequence_arm.go
@@ -8,5 +8,3 @@ func (this *Sequence) Store(value int64) {
 func (this *Sequence) Load() int64 {
 	return atomic.LoadInt64(&(*this)[SequencePayloadIndex])
 }
-
-const FillCPUCacheLine uint8 = 8 // TODO: how big is the cache line for ARM?
