80c288d265ecfc4d02ddb338bd3db5d1a7e9d8c9
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-16 21:35:11 -0600

    Removed machine-specific path.

--- example/run-arm.sh
+++ example/run-arm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-export PATH="$PATH:~/Downloads/android/adt-bundle-mac-x86_64-20140321/sdk/platform-tools"
-
 case "$1" in
 	copy)
 		GOOS=linux GOARCH=arm GOARM=7 go build -o go-disruptor
