76db312fb48d8d0609fb4ab18def8f227b37b8bc
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-17 09:29:59 -0600

    Only need android+ARM script.

--- example/amd64.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-go build -o go-disruptor
-./go-disruptor
--- example/android.sh
+++ example/android.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-case "$1" in
-	copy)
-		GOOS=linux GOARCH=arm GOARM=7 go build -o go-disruptor
-		adb push go-disruptor /data/local/tmp
-	;;
-esac
-
+GOOS=linux GOARCH=arm GOARM=7 go build -o go-disruptor
+adb push go-disruptor /data/local/tmp
 adb shell "cd /data/local/tmp; chmod 755 go-disruptor; ./go-disruptor"
