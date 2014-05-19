7d8453234d11a4197ae9d1c4ce7f20a70ee727b8
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-19 16:10:43 -0600

    Renamed files.

--- example/example_consumer.go
+++ example/example_consumer.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/smartystreets/go-disruptor"
 )
 
 const Mod = 1000000 * 10 // 1 million * N
 
-type ConsumerHandler struct{ started time.Time }
+func consume(writerBarrier disruptor.Barrier, writerCursor, readerCursor *disruptor.Cursor) {
+	reader := disruptor.NewReader(writerBarrier, &ConsumerHandler{time.Now()}, writerCursor, readerCursor)
 
+	for {
+		reader.Process()
+	}
+}
 func (this *ConsumerHandler) Consume(sequence, remaining int64) {
 	if sequence%Mod == 0 {
 		finished := time.Now()
@@ -19,3 +26,5 @@ func (this *ConsumerHandler) Consume(sequence, remaining int64) {
 		panic(fmt.Sprintf("Race condition--Cursor: %d, Message: %d\n", sequence, message))
 	}
 }
+
+type ConsumerHandler struct{ started time.Time }
