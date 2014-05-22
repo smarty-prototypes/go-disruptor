7da8d268ba7c1df5454eb68b782bdf5e5d006f5d
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-21 21:02:54 -0600

    Creating a specialized barrier to facilitate shared writes.

--- shared_writer.go
+++ shared_writer.go
@@ -1,49 +1,28 @@
 package disruptor
 
-import (
-	"math"
-	"sync/atomic"
-)
+import "sync/atomic"
 
 type SharedWriter struct {
 	capacity    int64
-	gate        int64 // TODO: this will most likely need to be a cursor
+	gate        int64 // TODO: determine if this should be a *Cursor
 	shift       uint8
 	committed   []int32
 	upstream    Barrier
 	reservation *Cursor
 }
 
-func NewSharedWriter(reservation *Cursor, capacity int64, upstream Barrier) *SharedWriter {
-	assertPowerOfTwo(capacity)
-
-	shift := uint8(math.Log2(float64(capacity)))
-	buffer := initializeCommittedBuffer(capacity)
-
+func NewSharedWriter(shared *SharedWriterBarrier, upstream Barrier) *SharedWriter {
 	return &SharedWriter{
-		capacity:    capacity,
+		capacity:    shared.capacity,
 		gate:        InitialSequenceValue,
-		shift:       shift,
-		committed:   buffer,
+		shift:       shared.shift,
+		committed:   shared.committed,
 		upstream:    upstream,
-		reservation: reservation,
+		reservation: shared.reservation,
 	}
-}
-func initializeCommittedBuffer(capacity int64) []int32 {
-	buffer := make([]int32, capacity)
-	for i := range buffer {
-		buffer[i] = int32(InitialSequenceValue)
-	}
-	return buffer
 }
 
 func (this *SharedWriter) Reserve(count int64) (int64, int64) {
-	if count <= 0 {
-		panic("Reservation must be a positive number.")
-	} else if count > this.capacity {
-		panic("Reservation cannot exceed the capacity.")
-	}
-
 	for {
 		previous := this.reservation.Load()
 		next := previous + count
@@ -68,16 +47,4 @@ func (this *SharedWriter) Commit(lower, upper int64) {
 	for mask := this.capacity - 1; lower <= upper; lower++ {
 		this.committed[lower&mask] = int32(lower >> this.shift)
 	}
-}
-
-func (this *SharedWriter) Load() int64 {
-	sequence := this.reservation.Load()
-
-	for mask := this.capacity - 1; sequence >= 0; sequence-- {
-		if this.committed[sequence&mask] == int32(sequence>>this.shift) {
-			return sequence
-		}
-	}
-
-	return sequence
 }
---
+++ shared_writer_barrier.go
@@ -0,0 +1,40 @@
+package disruptor
+
+import "math"
+
+type SharedWriterBarrier struct {
+	committed   []int32
+	capacity    int64
+	mask        int64
+	shift       uint8
+	reservation *Cursor
+}
+
+func NewSharedWriterBarrier(reservation *Cursor, capacity int64) *SharedWriterBarrier {
+	assertPowerOfTwo(capacity)
+
+	return &SharedWriterBarrier{
+		committed:   prepareCommitBuffer(capacity),
+		capacity:    capacity,
+		mask:        capacity - 1,
+		shift:       uint8(math.Log2(float64(capacity))),
+		reservation: reservation,
+	}
+}
+func prepareCommitBuffer(capacity int64) []int32 {
+	buffer := make([]int32, capacity)
+	for i := range buffer {
+		buffer[i] = int32(InitialSequenceValue)
+	}
+	return buffer
+}
+
+func (this *SharedWriterBarrier) Load() int64 {
+	for sequence := this.reservation.Load(); sequence >= 0; sequence-- {
+		if this.committed[sequence&this.mask] == int32(sequence>>this.shift) {
+			return sequence
+		}
+	}
+
+	return InitialSequenceValue
+}
