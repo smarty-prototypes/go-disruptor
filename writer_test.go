0c488d05a018201db79f1aa4508e3e1ce6328da1
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-30 16:59:29 -0600

    Added writer benchmark.

---
+++ writer_test.go
@@ -0,0 +1,17 @@
+package disruptor
+
+import "testing"
+
+func BenchmarkWriterReserve(b *testing.B) {
+	iterations := int64(b.N)
+	written, read := NewCursor(), NewCursor()
+	writer := NewWriter(written, read, 1024*64)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := int64(0); i < iterations; i++ {
+		writer.Reserve()
+		read.Sequence = i
+	}
+}
