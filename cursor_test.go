4726005ea6df9f63e4a75b19f5e0acf632952c97
Oliver, Jonathan <git@jonathanoliver.com>
2014-05-30 15:08:45 -0600

    Benchmarks for all cursors.

--- cursor_test.go
+++ cursor_test.go
@@ -2,7 +2,7 @@ package disruptor
 
 import "testing"
 
-func BenchmarkWrites(b *testing.B) {
+func BenchmarkCursorStore(b *testing.B) {
 	iterations := int64(b.N)
 
 	cursor := NewCursor()
@@ -12,5 +12,29 @@ func BenchmarkWrites(b *testing.B) {
 
 	for i := int64(0); i < iterations; i++ {
 		cursor.Store(i)
+	}
+}
+func BenchmarkCursorLoad(b *testing.B) {
+	iterations := int64(b.N)
+
+	cursor := NewCursor()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := int64(0); i < iterations; i++ {
+		cursor.Load()
+	}
+}
+func BenchmarkCursorRead(b *testing.B) {
+	iterations := int64(b.N)
+
+	cursor := NewCursor()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := int64(0); i < iterations; i++ {
+		cursor.Read(i)
 	}
 }
