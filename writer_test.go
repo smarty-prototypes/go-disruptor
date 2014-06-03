685d0adcd44e622bc6d90c4bd7f37e893d520de0
Oliver, Jonathan <git@jonathanoliver.com>
2014-06-03 12:38:03 -0600

    Additional writer tests.

--- writer_test.go
+++ writer_test.go
@@ -3,29 +3,51 @@ package disruptor
 import "testing"
 
 func BenchmarkWriterReserve(b *testing.B) {
+	read, written := NewCursor(), NewCursor()
+	writer := NewWriter(written, read, 1024)
 	iterations := int64(b.N)
-	written, read := NewCursor(), NewCursor()
-	writer := NewWriter(written, read, 1024*64)
-
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := int64(0); i < iterations; i++ {
+		sequence := writer.Reserve(1)
+		read.Store(sequence)
+	}
+}
+
+func BenchmarkWriterNextWrapPoint(b *testing.B) {
+	read, written := NewCursor(), NewCursor()
+	writer := NewWriter(written, read, 1024)
+	iterations := int64(b.N)
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	read.Store(1<<63 - 1)
+	for i := int64(0); i < iterations; i++ {
 		writer.Reserve(1)
-		read.sequence = i
 	}
 }
 
 func BenchmarkWriterAwait(b *testing.B) {
-	iterations := int64(b.N)
 	written, read := NewCursor(), NewCursor()
 	writer := NewWriter(written, read, 1024*64)
-
+	iterations := int64(b.N)
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for i := int64(0); i < iterations; i++ {
 		writer.Await(i)
-		read.sequence = i
+		read.Store(i)
+	}
+}
+
+func BenchmarkWriterCommit(b *testing.B) {
+	writer := NewWriter(NewCursor(), nil, 1024)
+	iterations := int64(b.N)
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := int64(0); i < iterations; i++ {
+		writer.Commit(i, i)
 	}
 }
