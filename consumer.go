725abe885dd0be051b754a5e5c3d88d88f7f6008
Oliver, Jonathan <git@jonathanoliver.com>
2014-06-12 15:08:52 -0600

    Updated TODO.

--- consumer.go
+++ consumer.go
@@ -1,6 +1,6 @@
 package disruptor
 
-// TODO: investigate performance impact of Consume(sequence, remaining int64) (consumed int64)
+// performance impact of Consume(sequence, remaining int64) (consumed int64)?
 type Consumer interface {
 	Consume(lower, upper int64)
 }
