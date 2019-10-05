ca137e9a2387b17effd58466773980668f9e40bf
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-10-05 09:07:17 -0600

    Inlined cursor type.

--- cursor.go
+++ cursor.go
@@ -2,20 +2,15 @@ package disruptor
 
 import "sync/atomic"
 
-type Cursor struct {
-	sequence int64
-	padding  [cpuCacheLinePadding]int64
-}
+type Cursor [cpuCacheLinePadding]int64
 
 func NewCursor() *Cursor {
-	return &Cursor{sequence: InitialSequenceValue}
+	this := &Cursor{}
+	this.Store(InitialSequenceValue)
+	return this
 }
 
-func (this *Cursor) Store(sequence int64) { atomic.StoreInt64(&this.sequence, sequence) }
-func (this *Cursor) Load() int64          { return atomic.LoadInt64(&this.sequence) }
+func (this *Cursor) Store(sequence int64) { atomic.StoreInt64(&this[0], sequence) }
+func (this *Cursor) Load() int64          { return atomic.LoadInt64(&this[0]) }
 
-const (
-	MaxSequenceValue     int64 = (1 << 63) - 1
-	InitialSequenceValue int64 = -1
-	cpuCacheLinePadding        = 7
-)
+const cpuCacheLinePadding = 8
--- interfaces.go
+++ interfaces.go
@@ -12,3 +12,8 @@ type Writer interface {
 	Reserve(count int64) int64
 	Commit(lower, upper int64)
 }
+
+const (
+	MaxSequenceValue     int64 = (1 << 63) - 1
+	InitialSequenceValue int64 = -1
+)
