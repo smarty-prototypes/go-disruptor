f25d8151cc3ce0f158ec42baff9db583136af556
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-10-05 11:17:56 -0600

    Rename.

--- reader.go
+++ reader.go
@@ -4,24 +4,24 @@ import "time"
 
 type Reader struct {
 	closed   *Cursor
-	reads    *Cursor // the reader has read up to this sequence
-	writes   *Cursor // the ring buffer has been written up to this sequence
+	read     *Cursor // the reader has read up to this sequence
+	written  *Cursor // the ring buffer has been written up to this sequence
 	upstream Barrier // the workers just in front of this reader have completed up to this sequence
 	consumer Consumer
 }
 
-func NewReader(reads, writes *Cursor, upstream Barrier, consumer Consumer) *Reader {
+func NewReader(read, written *Cursor, upstream Barrier, consumer Consumer) *Reader {
 	return &Reader{
 		closed:   NewCursor(),
-		reads:    reads,
-		writes:   writes,
+		read:     read,
+		written:  written,
 		upstream: upstream,
 		consumer: consumer,
 	}
 }
 
 func (this *Reader) Listen() {
-	current := this.reads.Load()
+	current := this.read.Load()
 	idling, gating := 0, 0
 
 	for {
@@ -30,9 +30,9 @@ func (this *Reader) Listen() {
 
 		if lower <= upper {
 			this.consumer.Consume(lower, upper)
-			this.reads.Store(upper)
+			this.read.Store(upper)
 			current = upper
-		} else if upper = this.writes.Load(); lower <= upper {
+		} else if upper = this.written.Load(); lower <= upper {
 			time.Sleep(time.Microsecond)
 			gating++
 			idling = 0
