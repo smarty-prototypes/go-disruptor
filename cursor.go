ad5f344c29efb6b328c59d78c53fb09ef57b65eb
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-10-05 09:34:24 -0600

    Inlined constant; revised constructor.

--- cursor.go
+++ cursor.go
@@ -2,15 +2,13 @@ package disruptor
 
 import "sync/atomic"
 
-type Cursor [cpuCacheLinePadding]int64
+type Cursor [8]int64 // prevent false sharing of the cursor by padding the CPU cache line with 64 *bytes* of data.
 
 func NewCursor() *Cursor {
-	this := &Cursor{}
-	this.Store(InitialCursorSequenceValue)
-	return this
+	var this Cursor
+	this[0] = InitialCursorSequenceValue
+	return &this
 }
 
 func (this *Cursor) Store(sequence int64) { atomic.StoreInt64(&this[0], sequence) }
 func (this *Cursor) Load() int64          { return atomic.LoadInt64(&this[0]) }
-
-const cpuCacheLinePadding = 8
