a3f80fda13135e0eb500935adb4da283b72098a9
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-10-05 11:53:36 -0600

    Unused interfaces.

--- composite_barrier.go
+++ composite_barrier.go
@@ -5,7 +5,7 @@ type CompositeBarrier []*Sequence
 func NewCompositeBarrier(sequences []*Sequence) CompositeBarrier { return sequences }
 
 func (this CompositeBarrier) Load() int64 {
-	var minimum = MaxCursorSequenceValue
+	var minimum = maxCursorSequenceValue
 
 	for _, item := range this {
 		if sequence := item.Load(); sequence < minimum {
@@ -15,3 +15,5 @@ func (this CompositeBarrier) Load() int64 {
 
 	return minimum
 }
+
+const maxCursorSequenceValue int64 = (1 << 63) - 1
--- interfaces.go
+++ interfaces.go
@@ -1,15 +1,5 @@
 package disruptor
 
-import "io"
-
-type Listener interface {
-	Listen()
-}
-type ListenCloser interface {
-	Listener
-	io.Closer
-}
-
 type Consumer interface {
 	Consume(lower, upper int64)
 }
@@ -27,5 +17,3 @@ type Sequencer interface {
 	Reserve(count int64) int64
 	Commit(lower, upper int64)
 }
-
-const MaxCursorSequenceValue int64 = (1 << 63) - 1
