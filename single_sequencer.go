42f2db385f04d1d6d5fba85055401f87b768a96b
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-10-05 11:09:04 -0600

    Moving power of two into wireup.

--- single_sequencer.go
+++ single_sequencer.go
@@ -10,11 +10,6 @@ type SingleSequencer struct {
 }
 
 func NewSingleSequencer(written *Cursor, upstream Barrier, capacity int64) *SingleSequencer {
-	if capacity <= 0 || (capacity&(capacity-1)) != 0 {
-		// Wikipedia entry: http://bit.ly/1krhaSB
-		panic("The ring capacity must be a power of two, e.g. 2, 4, 8, 16, 32, 64, etc.")
-	}
-
 	return &SingleSequencer{
 		upstream: upstream,
 		written:  written,
@@ -27,16 +22,14 @@ func (this *SingleSequencer) Reserve(count int64) int64 {
 	this.previous += count
 
 	for spin := int64(0); this.previous-this.capacity > this.upstream.Load(); spin++ {
-		if spin&SpinMask == 0 {
-			runtime.Gosched() // LockSupport.parkNanos(1L); http://bit.ly/1xiDINZ
+		if spin&spinMask == 0 {
+			runtime.Gosched() // http://bit.ly/1xiDINZ
 		}
 	}
 
 	return this.previous
 }
 
-func (this *SingleSequencer) Commit(_, upper int64) {
-	this.written.Store(upper)
-}
+func (this *SingleSequencer) Commit(_, upper int64) { this.written.Store(upper) }
 
-const SpinMask = 1024*16 - 1 // arbitrary; we'll want to experiment with different values
+const spinMask = 1024*16 - 1 // arbitrary; we'll want to experiment with different values
