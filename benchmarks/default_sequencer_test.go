3bbc41d683c0b2805cf655d02706704b686a51ef
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-10-12 08:46:46 -0600

    Inlined "shared" behavior which was only being used by a single benchmark suite.

--- benchmarks/default_sequencer_test.go
+++ benchmarks/default_sequencer_test.go
@@ -1,6 +1,7 @@
 package benchmarks
 
 import (
+	"log"
 	"math"
 	"testing"
 
@@ -77,3 +78,33 @@ func benchmarkSequencerReservations(b *testing.B, count int64, consumers ...disr
 
 	listener.Listen()
 }
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type SampleConsumer struct{}
+
+func (this SampleConsumer) Consume(lower, upper int64) {
+	var message int64
+	for lower <= upper {
+		message = ringBuffer[lower&RingBufferMask]
+		if message != lower {
+			log.Panicf("race condition: Sequence: %d, Message: %d", lower, message)
+		}
+		lower++
+	}
+}
+
+func build(consumers ...disruptor.Consumer) (disruptor.Sequencer, disruptor.ListenCloser) {
+	return disruptor.New(
+		disruptor.WithCapacity(RingBufferSize),
+		disruptor.WithConsumerGroup(consumers...))
+}
+
+const (
+	RingBufferSize = 1024 * 64
+	RingBufferMask = RingBufferSize - 1
+	ReserveOne     = 1
+	ReserveMany    = 16
+)
+
+var ringBuffer = [RingBufferSize]int64{}
