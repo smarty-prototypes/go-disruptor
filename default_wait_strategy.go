4b08104fe19deb91128f6754450c414204c75a82
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-10-12 08:02:41 -0600

    Adjusting sleep times.

--- default_wait_strategy.go
+++ default_wait_strategy.go
@@ -5,5 +5,5 @@ import "time"
 type DefaultWaitStrategy struct{}
 
 func NewWaitStrategy() DefaultWaitStrategy        { return DefaultWaitStrategy{} }
-func (this DefaultWaitStrategy) Gate(count int64) { time.Sleep(time.Microsecond) }
-func (this DefaultWaitStrategy) Idle(count int64) { time.Sleep(time.Microsecond) }
+func (this DefaultWaitStrategy) Gate(count int64) { time.Sleep(time.Nanosecond) }
+func (this DefaultWaitStrategy) Idle(count int64) { time.Sleep(time.Microsecond*50) }
