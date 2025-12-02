c35124623203427922c7ae7c3a27f0953b8f7e92
Jonathan Oliver <jonathan@smarty.com>
2025-12-02 12:39:44 -0700

    Multi-sequencer updates.

--- multi_sequencer.go
+++ multi_sequencer.go
@@ -6,23 +6,19 @@ type multiSequencer struct {
 	written   atomicSequence
 	gate      atomicSequence
 	upstream  sequenceBarrier
+	committed []int32
 	capacity  int64
 	shift     uint8
-	committed []int32
 }
-
-func newMultiSequencer(write *multiSequencerBarrier, upstream sequenceBarrier) *multiSequencer {
-	return &multiSequencer{
-		written:   write.written,
-		upstream:  upstream,
-		capacity:  write.capacity,
-		gate:      newSequence(),
-		shift:     write.shift,
-		committed: write.committed,
-	}
+type multiSequencerBarrier struct {
+	written   atomicSequence
+	committed []int32
+	capacity  int64
+	shift     uint8
 }
 
 func (this *multiSequencer) Reserve(count int64) int64 {
+	// blocks until desired number of slots becomes available
 	for {
 		previous := this.written.Load()
 		upper := previous + count
@@ -34,53 +30,67 @@ func (this *multiSequencer) Reserve(count int64) int64 {
 		if this.written.CompareAndSwap(previous, upper) {
 			return upper
 		}
+
+		// TODO: should we pass context.Context into this? if the caller aborts, we can skip the reservation request
 	}
 }
-
 func (this *multiSequencer) Commit(lower, upper int64) {
-	mask := this.capacity - 1
-
-	if lower == upper {
+	for mask := this.capacity - 1; upper >= lower; {
 		this.committed[upper&mask] = int32(upper >> this.shift)
-	} else {
-		// working down the array to keep items in the commit together; otherwise the reader(s) could split up the group
-		for upper >= lower {
-			this.committed[upper&mask] = int32(upper >> this.shift)
-			upper--
-		}
-	}
-}
-
-type multiSequencerBarrier struct {
-	written   atomicSequence
-	committed []int32
-	capacity  int64
-	shift     uint8
-}
-
-func newSharedWriterBarrier(written atomicSequence, capacity int64) *multiSequencerBarrier {
-	committed := make([]int32, capacity)
-	for i := range committed {
-		committed[i] = int32(defaultSequenceValue)
-	}
-
-	return &multiSequencerBarrier{
-		written:   written,
-		committed: committed,
-		capacity:  capacity,
-		shift:     uint8(math.Log2(float64(capacity))),
+		upper--
 	}
 }
 
 func (this *multiSequencerBarrier) Load(lower int64) int64 {
-	mask := this.capacity - 1
 	upper := this.written.Load()
 
-	for ; lower <= upper; lower++ {
+	for mask := this.capacity - 1; lower <= upper; lower++ {
 		if this.committed[lower&mask] != int32(lower>>this.shift) {
 			return lower - 1
 		}
 	}
 
 	return upper
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type multiSequencerConfiguration struct {
+	written   atomicSequence
+	committed []int32
+	capacity  int64
+	shift     uint8
+}
+
+func newMultiSequencerConfiguration(written atomicSequence, capacity uint32) *multiSequencerConfiguration {
+	committed := make([]int32, capacity)
+	for i := range committed {
+		committed[i] = int32(defaultSequenceValue)
+	}
+
+	return &multiSequencerConfiguration{
+		written:   written,
+		committed: committed,
+		capacity:  int64(capacity),
+		shift:     uint8(math.Log2(float64(capacity))),
+	}
+}
+
+func (this *multiSequencerConfiguration) NewBarrier() *multiSequencerBarrier {
+	return &multiSequencerBarrier{
+		written:   this.written,
+		committed: this.committed,
+		capacity:  this.capacity,
+		shift:     this.shift,
+	}
+}
+func (this *multiSequencerConfiguration) NewSequencer(upstream sequenceBarrier) Sequencer {
+	return &multiSequencer{
+		written:   this.written,
+		gate:      newSequence(),
+		upstream:  upstream,
+		committed: this.committed,
+		capacity:  this.capacity,
+		shift:     this.shift,
+	}
 }
