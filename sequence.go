c9cb270411828709b654d4051c789218cac7fba1
Jonathan Oliver <jonathan@smarty.com>
2026-03-02 14:21:21 -0700

    Confirm right-hand padding is necessary in Go.

--- sequence.go
+++ sequence.go
@@ -1,13 +1,16 @@
 package disruptor
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+	"unsafe"
+)
 
 // atomicSequence is a cache-line-padded atomic int64 used to track sequence positions without false sharing. The 56
 // bytes of padding on each side ensure the embedded atomic.Int64 occupies its own cache line.
 type atomicSequence struct {
-	_ [CacheLineBytes - 8]byte // 64B - 8B left padding
+	_ [CacheLineBytes - unsafe.Sizeof(int64(0))]byte // left padding: CPU padding bytes - int64
 	atomic.Int64
-	_ [CacheLineBytes - 8]byte // 64B - 8B right padding
+	// _ [CacheLineBytes - unsafe.Sizeof(int64(0))]byte // right padding: CPU padding bytes - int64
 }
 
 func newSequence() *atomicSequence {
