e321cd356b74f7cc0230ac9a470820797bec7715
Jonathan Oliver <jonathan@smarty.com>
2026-03-02 14:05:08 -0700

    Cache line size.

---
+++ cpu_architecture_64bit.go
@@ -0,0 +1,6 @@
+//go:build 386 || amd64 || arm64
+// +build 386 amd64 arm64
+
+package disruptor
+
+const CacheLineBytes = 64
--- sequence.go
+++ sequence.go
@@ -5,9 +5,9 @@ import "sync/atomic"
 // atomicSequence is a cache-line-padded atomic int64 used to track sequence positions without false sharing. The 56
 // bytes of padding on each side ensure the embedded atomic.Int64 occupies its own cache line.
 type atomicSequence struct {
-	_ [7]int64 // 56B left padding
+	_ [CacheLineBytes - 8]byte // 64B - 8B left padding
 	atomic.Int64
-	_ [7]int64 // 56B right padding
+	_ [CacheLineBytes - 8]byte // 64B - 8B right padding
 }
 
 func newSequence() *atomicSequence {
